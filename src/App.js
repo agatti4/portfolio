0c9e41f48e184251dc26bafe835c5cec498c6cb9
agatti4 <ajgatti4@gmail.com>
2022-05-26 13:57:28 -0400

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import NavBar from './NavBar.js';
+import Home from './pages/Home.js';
+import Pricing from './pages/Pricing.js';
+import About from './pages/About.js';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  return <NavBar />
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App;
